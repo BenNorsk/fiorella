aaf2e200592417ad3f003bc6819b86fbfaba67d9
Benjamin Brückner <69013582+BenNorsk@users.noreply.github.com>
2022-07-24 20:31:46 +0200

    Update install_fiorella.sh

--- install_fiorella.sh
+++ install_fiorella.sh
@@ -3,6 +3,6 @@ echo "Installing fiorella..."
 brew install brainfuck
 wget "https://raw.githubusercontent.com/BenNorsk/fiorella/273aa1fbe01768c6cd99438a70a369ddacb0a315/fiorella.py"
 mv fiorella.py ~/Documents/fiorella.py
-echo "alias fiorella = 'python  ~/Documents/fiorella.py'" >> ~/.zshsrc
+echo "alias fiorella = 'python  ~/Documents/fiorella/fiorella.py'" >> ~/.zshsrc
 echo "Installation complete."
 echo "Restart the terminal, and then use 'fiorella' to run the language." 
